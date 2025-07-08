a16803e2d0fe7b54105099d148aa99aae2c68d61
emgdesign <62917576+emgdesign@users.noreply.github.com>
2025-07-08 16:47:37 -0400

    Update streamlit_app.py
    adding parentheses to the .json addition

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,6 +58,6 @@ streamlit.title('My Parents New Healthy Diner')
 # New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json)
+st.text(smoothiefroot_response.json())
 
     

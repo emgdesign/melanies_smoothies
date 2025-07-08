25cfa6e29c92e2e25abbfcfa0b885f613be5894c
emgdesign <62917576+emgdesign@users.noreply.github.com>
2025-07-08 16:52:56 -0400

    Update streamlit_app.py
    adding the actual dataframe to the JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,6 +58,7 @@ streamlit.title('My Parents New Healthy Diner')
 # New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json(), use_container_width=True)
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     

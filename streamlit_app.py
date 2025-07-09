76efc66615f1199958300c7fddef4b85c9d20912
emgdesign <62917576+emgdesign@users.noreply.github.com>
2025-07-09 14:39:12 -0400

    Update streamlit_app.py
    adding ingredient_list multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,18 @@ if time_to_insert:
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-streamlit.title('My Parents New Healthy Diner')
+my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
+ingredient_list = st.multiselect()
+
+#streamlit.title('My Parents New Healthy Diner')
 
     
 

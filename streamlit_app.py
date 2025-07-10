0508a544eef7ca89a21ee8f7d5347bb63ccfef6d
emgdesign <62917576+emgdesign@users.noreply.github.com>
2025-07-10 15:24:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,11 @@ st.write(
   """
 )
 
-
 cnx = st.connection("snowflake")
 session = cnx.session()
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
-
-
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
@@ -34,17 +31,12 @@ ingredients_list = st.multiselect(
     , max_selections=5
     )
 
-
-
-
 if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ''
 
-  
-        
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       

55d8aa23075759481a62b630a20f5515d1e7a228
emgdesign <62917576+emgdesign@users.noreply.github.com>
2025-07-11 07:37:50 -0400

    Update streamlit_app.py
    moved name on order section up

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
+
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -13,8 +17,7 @@ st.write(
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-name_on_order = st.text_input('Name on Smoothie:')
-st.write('The name on your Smoothie will be:', name_on_order)
+
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)

ca40e8e752b706dd57db58d9589393e9c5287356
emgdesign <62917576+emgdesign@users.noreply.github.com>
2025-07-14 08:48:39 -0400

    Update streamlit_app.py
    added smoothiefroot back into the name of the responses

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,12 +43,12 @@ if ingredients_list:
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
         st.write(ingredients_string)
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        fv_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
               values ('""" + ingredients_string + """','""" + name_on_order + """')"""

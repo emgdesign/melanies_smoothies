6682ce5dece8b8eae970be336a6283cd2829d121
emgdesign <62917576+emgdesign@users.noreply.github.com>
2025-07-15 12:06:19 -0400

    Update streamlit_app.py
    closed up spaces and indents and made updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ import requests
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
   """Choose the fruits you want in your custom Smoothie!
-  """
-)
+  """)
+
 import streamlit as st
 import pandas as pd
 
@@ -17,7 +17,6 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()

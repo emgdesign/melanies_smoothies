006c3d07c1abb4043bb87b243c5d58107f224687
emgdesign <62917576+emgdesign@users.noreply.github.com>
2025-07-17 09:48:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-import pandas as pd
 from snowflake.snowpark.functions import col
 import requests
 
@@ -11,7 +10,7 @@ st.write(
   """)
 
 import streamlit as st
-
+import pandas as pd
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)

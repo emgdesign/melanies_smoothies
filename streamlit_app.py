fb982143299ddc0f8d887d91e3742f382015d80e
emgdesign <62917576+emgdesign@users.noreply.github.com>
2025-07-17 10:58:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
         #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
 
-#st.write(ingredients_string)
+st.write(ingredients_string)
 st.write(my_insert_stmt)
 st.stop()
 

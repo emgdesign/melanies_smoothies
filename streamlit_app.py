72af326ff0abedc476b95139ca3fc9c123509fd0
emgdesign <62917576+emgdesign@users.noreply.github.com>
2025-07-17 10:58:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,10 +47,12 @@ if ingredients_list:
         
 
 #st.write(ingredients_string)
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values('""" + ingredients_string + """','"""+name_on_order+ """')"""
-
 st.write(my_insert_stmt)
 st.stop()
+
+my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values('""" + ingredients_string + """','"""+name_on_order+ """')"""
+
+
 
 time_to_insert = st.button('Submit Order')
 
